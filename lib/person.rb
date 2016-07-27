8f6891156b6eafc8eebb9f930a57638f33b784a3
Veronica Li <vronnieli@gmail.com>
2016-07-27 10:18:53 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name= (full_name)
+    @name = full_name
+  end
+
+  def name
+    @name
+  end
+
+  def job= (occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+
+end
